d18f4a2f784d579999ff9df8fa7be06695aa63bd
Hieu <hieu.nd1995@gmail.com>
2020-08-13 23:53:01 -0400

    update difficulty

--- js/index.js
+++ js/index.js
@@ -272,7 +272,7 @@ function findEmpty(grid) {
 // random a new grid
 function randomSudoku() {
   a = SudokuCreate(9);
-  delNum = Math.floor((Math.random() * 1) + 1);
+  delNum = Math.floor((Math.random() * 25) + 21);
   let i = 0;
 
   let exist = [[]];
